eca10a0f818a548ab29b8d2539df770ef163f4e1
willieLjohnson <liwa.johnson@gmail.com>
2019-05-11 20:12:10 -0400

    Basic project setup

---
+++ Assets/Scripts/Utilities/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+  public class Helper : MonoBehaviour
+  {
+    [Range(0, 1)]
+    public float vertical;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      animator.SetFloat("vertical", vertical);
+      print(animator.GetFloat("vertical"));
+    }
+  }
+}
+
