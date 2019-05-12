f844ba10e00370074b1728595d85aa6d64ee8348
willieLjohnson <liwa.johnson@gmail.com>
2019-05-11 20:48:29 -0400

    Add attack animation with animation player script

--- Assets/Scripts/Utilities/Helper.cs
+++ Assets/Scripts/Utilities/Helper.cs
@@ -7,7 +7,13 @@ namespace SA
   public class Helper : MonoBehaviour
   {
     [Range(0, 1)]
+    // Vertical speed of player.
     public float vertical;
+
+    // Name of animation currently playing.
+    public string animationName;
+    // Should animation play?
+    public bool playAnimation;
 
     Animator animator;
 
@@ -20,8 +26,13 @@ namespace SA
     // Update is called once per frame
     void Update()
     {
+      if (playAnimation)
+      {
+        vertical = 0;
+        animator.CrossFade(animationName, 0.2f);
+        playAnimation = false;
+      }
       animator.SetFloat("vertical", vertical);
-      print(animator.GetFloat("vertical"));
     }
   }
 }
