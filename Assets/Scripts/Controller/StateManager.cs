29c7212135a5ced0193d2cc6d3af1a3c6d61a43f
willieLjohnson <liwa.johnson@gmail.com>
2019-05-20 21:56:09 -0400

    Add player movement

--- Assets/Scripts/Controller/StateManager.cs
+++ Assets/Scripts/Controller/StateManager.cs
@@ -4,12 +4,22 @@ namespace SA
 {
   public class StateManager : MonoBehaviour
   {
+    [Header("Init")]
+    public GameObject activeModel;
+
+    [Header("Inputs")]
     public float vertical;
     public float horizontal;
     public float moveAmount;
     public Vector3 moveDirection;
 
-    public GameObject activeModel;
+    [Header("Stats")]
+    public float moveSpeed = 2;
+    public float runSpeed = 3.5f;
+
+    [Header("States")]
+    public bool run;
+
     [HideInInspector]
     public Animator animator;
     [HideInInspector]
@@ -48,7 +58,11 @@ namespace SA
     {
       this.delta = delta;
 
-      rigidbody.velocity = moveDirection;
+      float targetSpeed = moveSpeed;
+      if (run)
+        targetSpeed = runSpeed;
+
+      rigidbody.velocity = moveDirection * targetSpeed;
     }
   }
 }
