f2d835a260266f05e7664703b15b6c8c6b0de16f
willieLjohnson <liwa.johnson@gmail.com>
2019-05-20 21:07:25 -0400

    Init and update cameramanager in inputhandler

--- Assets/Scripts/Controller/InputHandler.cs
+++ Assets/Scripts/Controller/InputHandler.cs
@@ -10,18 +10,31 @@ namespace SA
     float horizontal;
 
     StateManager stateManager;
+    CameraManager cameraManager;
+
+    float delta;
 
     // Start is called before the first frame update
     void Start()
     {
       stateManager = GetComponent<StateManager>();
       stateManager.Init();
+
+      cameraManager = CameraManager.singleton;
+      cameraManager.Init(this.transform);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+      delta = Time.fixedDeltaTime;
       GetInput();
+    }
+
+    void Update()
+    {
+      delta = Time.deltaTime;
+      cameraManager.Tick(delta);
     }
 
     void GetInput()
@@ -35,7 +48,7 @@ namespace SA
       stateManager.horizontal = horizontal;
       stateManager.vertical = vertical;
 
-      stateManager.Tick(Time.deltaTime);
+      stateManager.Tick(delta);
     }
   }
 
