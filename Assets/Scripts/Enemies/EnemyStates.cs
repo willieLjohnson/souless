672d4287aa8c449334bd92015f3e1c98495b81f2
willieLjohnson <liwa.johnson@gmail.com>
2019-05-27 13:21:51 -0400

    Add damage colliders with damage animation on enemy

--- Assets/Scripts/Enemies/EnemyStates.cs
+++ Assets/Scripts/Enemies/EnemyStates.cs
@@ -6,7 +6,8 @@ namespace SA
 {
   public class EnemyStates : MonoBehaviour
   {
-    
+    public float health;
+    public bool isInvisible;
     Animator animator;
     EnemyTarget enemyTarget;
 
@@ -15,6 +16,24 @@ namespace SA
       animator = GetComponentInChildren<Animator>();
       enemyTarget = GetComponent<EnemyTarget>();
       enemyTarget.Init(animator);
+    }
+
+    void Update()
+    {
+      if (isInvisible)
+      {
+        isInvisible = !animator.GetBool("can_move");
+      }
+    }
+
+    public void DoDamage(float value)
+    {
+      if (isInvisible)
+        return;
+
+      health -= value;
+      isInvisible = true;
+      animator.Play("damage_1");
     }
   }
 }
